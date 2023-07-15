121a3af17b2cd227484abf2b76b04cf461228fef
xaviergeorge <13365640+xaviergeorge@users.noreply.github.com>
2023-07-15 12:45:14 +0200

    printing info before download

--- mapping/scarches_api/utils/utils.py
+++ mapping/scarches_api/utils/utils.py
@@ -396,6 +396,7 @@ def fetch_file_from_s3(key, path):
     client = boto3.client('s3', endpoint_url=os.getenv('AWS_ENDPOINT'),
                           aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
                           aws_secret_access_key=os.getenv('AWS_SECRET_KEY'))
+    print("fetching file from s3 with Bucket. key. path ", os.getenv('AWS_BUCKET'), key, path)
     client.download_file(os.getenv('AWS_BUCKET'), key, path)
 
 
