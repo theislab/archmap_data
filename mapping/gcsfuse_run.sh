ff62d10203e20bf9a77adadbd628de636fe00441
xaviergeorge <13365640+xaviergeorge@users.noreply.github.com>
2023-09-05 19:21:24 +0530

    add gcsfuse_run

--- mapping/gcsfuse_run.sh
+++ mapping/gcsfuse_run.sh
@@ -1,47 +1,28 @@
-FROM python:3.9-slim
+
+#!/bin/bash
+# set -eo pipefail;
+
+# Create mount directory for service
+mkdir -p $MNT_DIR;
+
+pwd;
+echo "Mounting GCS Fuse for bucket $AWS_BUCKET to $MNT_DIR"
+gcsfuse  --implicit-dirs  $AWS_BUCKET $MNT_DIR 
+echo "Mounting completed."
+echo "Listing the mount directory."
+ls -l $MNT_DIR;
 
 
-# Install system dependencies
-RUN set -e; \
-    apt-get update -y && apt-get install -y \
-    tini \
-    lsb-release curl gnupg2; \
-    gcsFuseRepo=gcsfuse-`lsb_release -c -s`; \
-    echo "deb http://packages.cloud.google.com/apt $gcsFuseRepo main" | \
-    tee /etc/apt/sources.list.d/gcsfuse.list; \
-    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | \
-    apt-key add -; \
-    apt-get update; \
-    apt-get install -y gcsfuse \
-    && apt-get clean 
-
-# Install required packages
-RUN apt-get update && \
-    apt-get install -y gcc && \
-    apt-get clean
-
-ENV APP_HOME /app
-WORKDIR $APP_HOME
 
 
-ENV MNT_DIR /mnt/gcs
 
-ADD ./scarches_api/requirements.txt .
+# cd into ${MNT_DIR}/${FULL_PATH}/ 
+# echo "changing directory to ${MNT_DIR}/${FULL_PATH}/"
+# cd ${MNT_DIR}/${FULL_PATH}/
 
-RUN pip install -r requirements.txt 
-
-COPY . .
-
-RUN chmod +x ${APP_HOME}/gcsfuse_run.sh
+echo "the pwd is now: "
+pwd;
 
 
-ENV WORKERS 1
-ENV THREADS 1
-ENV PORT 8080
-ENV PYTHONPATH $APP_HOME
-
-# Use tini to manage zombie processes and signal forwarding
-# https://github.com/krallin/tini
-ENTRYPOINT ["/usr/bin/tini", "--"]
-
-CMD ["./gcsfuse_run.sh"]
+#run the api
+gunicorn --bind :$PORT --workers $WORKERS --threads $THREADS --timeout 0 --chdir ./scarches_api/ api:app
