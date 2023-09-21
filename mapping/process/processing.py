cc9c28da73c87e91de40f9d7c575406e8e88b4a8
Farhad <m.mogharehdehkordi@gmail.com>
2023-09-21 14:27:53 +0200

    reversed hardcoded path

--- mapping/process/processing.py
+++ mapping/process/processing.py
@@ -244,10 +244,15 @@ class Preprocess:
         print(utils.get_from_config(configuration, parameters.QUERY_DATA_PATH))
         # hardcode the query path
 
-        print("pausing for 10 min...")
+        # print("pausing for 10 min...")
         # import time
         # time.sleep(600)
-        target_adata = utils.read_h5ad_file_from_s3("query_test_data/query_romanov_reworked.h5ad")
+
+        # hardcoded for testing hypomap
+        # target_adata = utils.read_h5ad_file_from_s3("query_test_data/query_romanov_reworked.h5ad")
+       
+        target_adata = utils.read_h5ad_file_from_s3(utils.get_from_config(configuration, parameters.QUERY_DATA_PATH))
+       
         #TODO: HARDCODING---------------------------------------------------
         # if utils.get_from_config(configuration, parameters.ATLAS) == 'human_lung':
         #     X_train = source_adata.X
