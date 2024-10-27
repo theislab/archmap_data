5c7c52475c1b2788504bdac995bb4d5b57373011
ChelseaBright96 <chels.alex.bright@gmail.com>
2024-10-27 16:55:12 +0100

    fix bug

--- mapping/scvi_hub/scvi_hub.py
+++ mapping/scvi_hub/scvi_hub.py
@@ -158,7 +158,7 @@ class ScviHub:
         print("save")
         self._save_data()
 
-        utils.notify_backend(get_from_config(self._configuration, parameters.WEBHOOK), self._configuration)
+        utils.notify_backend(utils.get_from_config(self._configuration, parameters.WEBHOOK), self._configuration)
     
 
 
