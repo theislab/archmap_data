4cc9c8611a25d30807712cc2f7e41760de7d96ac
ChelseaBright96 <chels.alex.bright@gmail.com>
2025-01-25 18:58:41 +0100

    test webhook

--- mapping/scarches_api/test.py
+++ mapping/scarches_api/test.py
@@ -31,52 +31,54 @@ def notify_backend(endpoint, payload):
 
 def main():
 
-    print(os.environ) # show all environment variables and their values.
+    # print(os.environ) # show all environment variables and their values.
 
-    modelPath = os.getenv('modelpath')
-    atlasPath = os.getenv('atlaspath')
-    modelName = os.getenv('modelname')
-    batchkey = os.getenv('batchkey')
-    celltypekey = os.getenv('celltypekey')
-    atlasName = os.getenv('atlasname')
+    # modelPath = os.getenv('modelpath')
+    # atlasPath = os.getenv('atlaspath')
+    # modelName = os.getenv('modelname')
+    # batchkey = os.getenv('batchkey')
+    # celltypekey = os.getenv('celltypekey')
+    # atlasName = os.getenv('atlasname')
     webhook = os.getenv('webhook')
 
-    print(f"modelpath: {modelPath}")
-    print(f"atlaspath: {atlasPath}")
+    print(webhook)
 
-    #Get model and data
-    modelfile_gcp = f"models/{modelPath}/model.pt"
-    adatafile_gcp = f"atlas/{atlasPath}/data.h5ad"
+    # print(f"modelpath: {modelPath}")
+    # print(f"atlaspath: {atlasPath}")
 
-    directory="model/"
-    if not os.path.exists(directory):
-        os.makedirs(directory, exist_ok=True)
+    # #Get model and data
+    # modelfile_gcp = f"models/{modelPath}/model.pt"
+    # adatafile_gcp = f"atlas/{atlasPath}/data.h5ad"
 
-    modelfile_local = "model/model.pt"
-    adatafile_local = "model/adata.h5ad"
+    # directory="model/"
+    # if not os.path.exists(directory):
+    #     os.makedirs(directory, exist_ok=True)
 
-    fetch_file_from_s3(modelfile_gcp, modelfile_local)
-    fetch_file_from_s3(adatafile_gcp, adatafile_local)
+    # modelfile_local = "model/model.pt"
+    # adatafile_local = "model/adata.h5ad"
 
-    modelpath_local = "model"
+    # fetch_file_from_s3(modelfile_gcp, modelfile_local)
+    # fetch_file_from_s3(adatafile_gcp, adatafile_local)
+
+    # modelpath_local = "model"
 
 
-    # TODO: 
-    # Check that data is not minified
+    # # TODO: 
+    # # Check that data is not minified
 
-    # benchmark integration
-    benchmark(modelName, atlasName, modelpath_local, batchkey, celltypekey, modelPath)
-    benchmark_plot(atlasName, modelName, batchkey, celltypekey, modelPath)
+    # # benchmark integration
+    # benchmark(modelName, atlasName, modelpath_local, batchkey, celltypekey, modelPath)
+    # benchmark_plot(atlasName, modelName, batchkey, celltypekey, modelPath)
     
-    # minify
-    minify(modelName, atlasName, modelpath_local, modelPath, atlasPath)
+    # # minify
+    # minify(modelName, atlasName, modelpath_local, modelPath, atlasPath)
 
-    # get classifiers and uncert
-    adata = classify(atlasName, modelName, celltypekey, modelPath)
-    store_results(atlasName, celltypekey, modelPath)
+    # # get classifiers and uncert
+    # adata = classify(atlasName, modelName, celltypekey, modelPath)
+    # store_results(atlasName, celltypekey, modelPath)
 
 
-    uncertainty_train(atlasName, adata, modelName, celltypekey, modelPath)
+    # uncertainty_train(atlasName, adata, modelName, celltypekey, modelPath)
 
     # notify backend that benchmarking completed
     notify_backend(webhook, {})
