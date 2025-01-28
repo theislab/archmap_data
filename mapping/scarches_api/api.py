aa136d4de3041cc5641675cd54298e0d2436ba8e
ChelseaBright96 <chels.alex.bright@gmail.com>
2025-01-28 08:58:10 +0100

    test job

--- mapping/scarches_api/api.py
+++ mapping/scarches_api/api.py
@@ -1,11 +1,11 @@
-from flask import Flask, request
+# from flask import Flask, request
 import os
 import init as scarches
 from threading import Thread
 from scarches_api.utils import utils, parameters
 import traceback
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
 
 def get_from_config(configuration, key):
@@ -13,11 +13,10 @@ def get_from_config(configuration, key):
         return configuration[key]
     return None
 
-
-@app.route('/query', methods=['POST'])
-def query():
+def main():
     try:
-        config = request.get_json(force=True)
+        config = os.environ
+        print(config)
         run_async = get_from_config(config, parameters.RUN_ASYNCHRONOUSLY)
         if run_async is not None and run_async:
             actual_config = scarches.merge_configs(config)
@@ -42,10 +41,41 @@ def query():
         return {'error': str(e)}, 500
 
 
-@app.route("/liveness")
-def liveness():
-    return "up"
+
+# @app.route('/query', methods=['POST'])
+# def query():
+#     try:
+#         config = request.get_json(force=True)
+#         run_async = get_from_config(config, parameters.RUN_ASYNCHRONOUSLY)
+#         if run_async is not None and run_async:
+#             actual_config = scarches.merge_configs(config)
+#             # thread = Thread(target=scarches.query, args=(config,))
+#             # thread.start()
+#             actual_config = scarches.query(config)
+#             return actual_config, 200
+#         else:
+#             actual_configuration = scarches.query(config)
+#             return actual_configuration, 200
+#     except Exception as e:
+#         print("Error in query\n")
+#         traceback.print_exc()
+#         if e is not None:
+#             if len(str(e)) > 0:
+#                 utils.notify_backend(get_from_config(config, parameters.WEBHOOK), {'error': str(e)})
+#             else:
+#                 utils.notify_backend(get_from_config(config, parameters.WEBHOOK), {'error': "Unknown error"})
+#         else:
+#             utils.notify_backend(get_from_config(config, parameters.WEBHOOK), {'error': "Unknown error"})
+        
+#         return {'error': str(e)}, 500
+
+
+# @app.route("/liveness")
+# def liveness():
+#     return "up"
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    main()
+
+    # app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
