0c76f21627e6ff18b71788a7f8864e80e95d51a0
ChelseaBright96 <chels.alex.bright@gmail.com>
2025-01-29 09:56:43 +0100

    fix bug

--- mapping/scarches_api/api.py
+++ mapping/scarches_api/api.py
@@ -21,7 +21,9 @@ def main():
 
         # convert listed vars
         config = config.replace("'", '"')
-        vars_to_convert = json.loads(config["vars_to_convert"])
+        vars_to_convert = config["vars_to_convert"]
+        vars_to_convert = vars_to_convert.replace("'", '"')
+        vars_to_convert = json.loads(vars_to_convert)
         for key, values in vars_to_convert.keys():
             if key=="dictionary":
                 for v in values:
