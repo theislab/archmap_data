84f352fa93ec588019fbb5fe12ec8dde52fbbf2c
ChelseaBright96 <chels.alex.bright@gmail.com>
2025-02-09 08:44:12 +0100

    fix bug scanvi atlas upload

--- mapping/process/processing.py
+++ mapping/process/processing.py
@@ -436,7 +436,7 @@ class Preprocess:
         else:
             batch_key = utils.get_from_config(configuration, parameters.BATCH_KEY)
             cell_type_key = utils.get_from_config(configuration, parameters.CELL_TYPE_KEY)
-            cell_type_key = [cell_type_key]
+            cell_type_key_classifier = [cell_type_key]
             uploaded = True
 
             
