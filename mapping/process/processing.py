e108ae4c37b0b5c6d9993ccda5d131bb1ef5fc08
ChelseaBright96 <chels.alex.bright@gmail.com>
2025-06-05 10:00:29 +0200

    change cell type keys heoca

--- mapping/process/processing.py
+++ mapping/process/processing.py
@@ -413,8 +413,8 @@ class Preprocess:
                                     'annot_ntt_rev2',]
             batch_key = "batch"
         elif atlas == "heoca":
-            cell_type_key = "cell_type"
-            cell_type_key_classifier = ['level_2']
+            cell_type_key = ["level_1",'level_2']
+            cell_type_key_classifier = ["level_1",'level_2']
             batch_key = "sample_id"
         elif atlas == "fetal_brain":
             cell_type_key = "subregion_class"
