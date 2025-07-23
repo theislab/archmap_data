3f327d883e4b9b0a27660d1a37095f4a99f44347
ChelseaBright96 <chels.alex.bright@gmail.com>
2025-07-23 07:24:32 +0200

    raise error empty X

--- mapping/scarches_api/utils/utils.py
+++ mapping/scarches_api/utils/utils.py
@@ -886,9 +886,7 @@ def check_h5ad_format(query):
     # make sure query count does not contain all zeros
 
     if not query.X.sum() > 0:
-        try: 
-            query.X = query.raw.X
-        except ValueError as e:
+        if query.raw is None:
             raise ValueError("The count matrix of the query contains only zeros. Please make sure that the count matrix is stored in the .X attribute of your h5ad file.")
 
 
