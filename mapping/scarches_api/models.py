339964a537bbbe01e7f87af78f7158deaf54ef14
Chelsea Bright <chels.alex.bright@gmail.com>
2025-10-13 20:05:48 +0200

    change sampling procedure

--- mapping/scarches_api/models.py
+++ mapping/scarches_api/models.py
@@ -463,7 +463,7 @@ class ArchmapBaseModel():
             count_matrix_size_gb = self.add_X_from_cloud()
 
             if count_matrix_size_gb<40:
-                if self._combined_adata.n_obs>3000000:
+                if self._combined_adata.n_obs>100000:
                     combined_downsample = self.downsample_adata()
                 else:
                     combined_downsample = self._combined_adata.copy()
