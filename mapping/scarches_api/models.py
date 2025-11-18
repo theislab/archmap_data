72b18efd6fc140e4301d4eb549e2aeb791f9a783
Chelsea Bright <chels.alex.bright@gmail.com>
2025-11-18 10:39:04 +0100

    add steps to logs

--- mapping/scarches_api/models.py
+++ mapping/scarches_api/models.py
@@ -65,7 +65,7 @@ class ArchmapBaseModel():
         self._combined_adata = None
         self.percent_unknown = "n/a"
 
-        utils.notify_backend(self._webhook_progress, {"logs":"fetching and preprocessing data"})
+        utils.notify_backend(self._webhook_progress, {"logs":"Step 1/6: fetching and preprocessing data"})
 
         #Load and process required data
         self._acquire_data()
@@ -148,7 +148,7 @@ class ArchmapBaseModel():
 
     def _map_query(self):
         #Map the query onto reference
-        utils.notify_backend(self._webhook_progress, {"logs":"mapping query"})
+        utils.notify_backend(self._webhook_progress, {"logs":"Step 2/6: mapping query"})
         
 
         # threshold = 10000
@@ -275,7 +275,7 @@ class ArchmapBaseModel():
         
 
     def _eval_mapping(self):
-        utils.notify_backend(self._webhook_progress, {"logs":"calculating mapping uncertainty scores"})
+        utils.notify_backend(self._webhook_progress, {"logs":"Step 3/6: calculating mapping uncertainty scores"})
         #Create AnnData objects off the latent representation
         query_latent = scanpy.AnnData(self._query_adata.obsm["latent_rep"])
         reference_latent = scanpy.AnnData(self._reference_adata.obsm["latent_rep"])
@@ -291,7 +291,7 @@ class ArchmapBaseModel():
             stress_score(self._query_adata)
 
     def _transfer_labels(self):
-        utils.notify_backend(self._webhook_progress, {"logs":"transferring labels from reference to query"})
+        utils.notify_backend(self._webhook_progress, {"logs":"Step 4/6: transferring labels from reference to query"})
         if not self._clf_native and not self._clf_knn and not self._clf_xgb:
             return
         
@@ -362,7 +362,7 @@ class ArchmapBaseModel():
                         os.remove(self._temp_clf_encoding_path)
 
     def _concat_data(self):
-        utils.notify_backend(self._webhook_progress, {"logs":"concatenating reference and query results"})
+        utils.notify_backend(self._webhook_progress, {"logs":"Step 5/6: concatenating reference and query results"})
         #save .X and var_names of query in new adata for later concatenation after cellxgene
         self.adata_query_X = scanpy.AnnData(self._query_adata.X.copy())
         self.adata_query_X.var_names = self._query_adata.var_names
@@ -446,7 +446,7 @@ class ArchmapBaseModel():
         explicit_representation.obsm["latent_rep"] = self._model.get_latent_representation(explicit_representation)
 
     def _save_data(self):
-        utils.notify_backend(self._webhook_progress, {"logs":"calculating mapping evaluation metrics and saving results"})
+        utils.notify_backend(self._webhook_progress, {"logs":"Step 6/6: calculating mapping evaluation metrics and saving results"})
         # add .X to self._combined_adata
 
         if self.batch_key_input != self._batch_key:
